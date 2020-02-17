7c501d7a69325e3bbfda23512becc9ef8882415c
Veruska Santos <veruska.santos@ccc.ufcg.edu.br>
2020-02-17 08:31:00 -0300

    Incremental Learning and config dependencies

---
+++ workspace/config.py
@@ -0,0 +1,14 @@
+# Dependencies
+
+pip3 install numpy
+pip3 install pandas
+pip3 install xgboost
+pip3 install catboost
+pip3 install https://github.com/JamesRitchie/scikit-rvm/archive/master.zip
+
+pip3 install deap update_checker tqdm stopit
+pip3 install tpot
+
+sudo apt-get install build-essential swig
+curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt | xargs -n 1 -L 1 pip3 install
+pip3 install auto-sklearn
